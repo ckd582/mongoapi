b6576cd4403425602df1b23cce278dc22e6a2e0f
unknown <qud582@gmail.com>
2020-10-05 11:29:22 +0900

    first comit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function (request, response) {
+  console.log(request);
+  response.send("Hello World");
+});
+
+var port = 8080;
+app.listen(port, function () {
+  console.log(`Server is Starting at http://localhost:${port}`);
+});
